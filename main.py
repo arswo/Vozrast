a4d136daab84df220e1d06226b73d461e63bf415
arswo <aloewera228@mail.ru>
2020-03-24 23:55:56 +0300

    init_1

---
+++ main.py
@@ -0,0 +1,13 @@
+print('''vvedi parol': ''')
+parol =	input()
+
+if age == str(18):
+    print('ti norm')
+
+elif age >= str(19):
+		print ('you big boy')
+
+elif age <= str(17):
+		print ('you small boy')
+
+input()
